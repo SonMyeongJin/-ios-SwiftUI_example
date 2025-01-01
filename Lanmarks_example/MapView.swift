4d01b6231ef5c7a3826f87f51962bea9b8dd73b4
SonMyeongJin <smj2802@naver.com>
2025-01-02 00:41:24 +0900

    MapKit

---
+++ Lanmarks_example/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  Lanmarks_example
+//
+//  Created by 손명진 on 1/2/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+
+    }
+    
+    private var region: MKCoordinateRegion {
+           MKCoordinateRegion(
+               center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+               span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+           )
+       }
+}
+
+#Preview {
+    MapView()
+}
