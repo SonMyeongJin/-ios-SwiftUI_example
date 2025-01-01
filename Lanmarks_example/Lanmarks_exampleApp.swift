7765c1b0633461defc8051caadc331103d74634c
SonMyeongJin <smj2802@naver.com>
2025-01-01 23:39:46 +0900

    Initial Commit

---
+++ Lanmarks_example/Lanmarks_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lanmarks_exampleApp.swift
+//  Lanmarks_example
+//
+//  Created by 손명진 on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Lanmarks_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
