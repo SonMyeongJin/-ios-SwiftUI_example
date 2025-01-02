fbc3df138cf856934ce3d59dbc7fd8c1e0cb6e26
SonMyeongJin <smj2802@naver.com>
2025-01-02 17:45:16 +0900

    지도뷰 + 제목텍스트 뷰+ 사진 뷰

--- Lanmarks_example/ContentView.swift
+++ Lanmarks_example/ContentView.swift
@@ -9,18 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .leading){
-            Text("Landmarks Rock")
-                .font(.title)
-                .foregroundStyle(Color.green)
-            HStack {
-                Text("Joshua Tree National park")
-                    .font(.subheadline)
-                Spacer()
-                Text("South of Korea")
-                    .font(.subheadline)
-            }
-        }.padding()
+        VStack {
+            MapView()
+                .frame(height:300)
+            
+            Circle_Image()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading){
+                Text("Landmarks Rock")
+                    .font(.title)
+                    .foregroundStyle(Color.green)
+                HStack {
+                    Text("Joshua Tree National park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("South of Korea")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
+            }.padding()
+            
+            Spacer()
+            
+            
+        }
     }
 }
 
