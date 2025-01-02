71d985eb387ce3b0cee94eb6694422d186849c90
SonMyeongJin <smj2802@naver.com>
2025-01-02 18:35:12 +0900

    json 자료 데이터 파일 읽어오는 로직 구현

---
+++ Lanmarks_example/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  Lanmarks_example
+//
+//  Created by 손명진 on 1/2/25.
+//
+
+import Foundation
---
+++ Lanmarks_example/Model/Landmark.swift
@@ -0,0 +1,36 @@
+//
+//  Landmark.swift
+//  Lanmarks_example
+//
+//  Created by 손명진 on 1/2/25.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable,Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var Description: String
+    
+    private var imageName: String
+    var image: Image{
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinate
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+            CLLocationCoordinate2D(
+                latitude: coordinates.latitude,
+                longitude: coordinates.longitude)
+        }
+
+    struct Coordinate: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
