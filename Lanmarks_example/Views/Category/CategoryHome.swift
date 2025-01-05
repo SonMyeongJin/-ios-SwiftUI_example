1ca6123bafa6eb7c5c3bc070a5967b7792f29c31
SonMyeongJin <smj2802@naver.com>
2025-01-05 23:34:17 +0900

    카테고리 홈 로직구성

---
+++ Lanmarks_example/Views/Category/CategoryHome.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryHome.swift
+//  Lanmarks_example
+//
+//  Created by 손명진 on 1/4/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
